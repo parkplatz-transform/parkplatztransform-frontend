81fd1b9a70fa6c35dba959323e21e1ffbb8b317d
Theo Lampert <mail@theo.sh>
2021-01-13 10:05:42 +0100

    chore: add sentry monitoring

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
