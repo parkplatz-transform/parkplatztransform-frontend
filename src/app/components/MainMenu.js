321a1a04cf4a97741e2acf948deedbbb910d52e6
Vanessa <38529028+van-pertsch@users.noreply.github.com>
2021-02-26 10:33:54 +0100

    Update MainMenu.js
    add IconButton from '@material-ui/core/IconButton'

--- src/app/components/MainMenu.js
+++ src/app/components/MainMenu.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-
+import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
