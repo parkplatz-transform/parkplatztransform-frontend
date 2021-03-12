494d9a7834f887a66a34e5ace9110ad16fe67975
Vanessa Pertsch <vanessa.pertsch@posteo.de>
2021-03-12 14:10:08 +0100

    add strings for howto on rightPanel

--- src/app/components/RightPanel.js
+++ src/app/components/RightPanel.js
@@ -1,5 +1,5 @@
-import React from "react"
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
@@ -11,62 +11,92 @@ const useStyles = makeStyles({
     margin: '20px auto',
     textAlign: 'center',
     fontEeight: 'bold',
-    fontSize: 20
+    fontSize: 20,
   },
   subheader: {
     margin: '20px auto',
     textAlign: 'center',
     fontEeight: 'bold',
-    fontSize: 16
+    fontSize: 16,
   },
   verticalSpace: {
-    height: 30
+    height: 30,
   },
   loadingContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '100%'
-  }
+    height: '100%',
+  },
 })
 
-function RightPanel({ isLoading, segment, onSegmentChanged, setAlertDisplayed }) {
-    const classes = useStyles()
+function RightPanel({
+  isLoading,
+  segment,
+  onSegmentChanged,
+  setAlertDisplayed,
+}) {
+  const classes = useStyles()
 
-    function onValidationFailed(message) {
-        setAlertDisplayed({severity: 'error', message})
-    }
+  function onValidationFailed(message) {
+    setAlertDisplayed({ severity: 'error', message })
+  }
 
-    if (isLoading && !segment) {
-      return (
-        <div className={classes.loadingContainer}>
-          <CircularProgress/>
+  if (isLoading && !segment) {
+    return (
+      <div className={classes.loadingContainer}>
+        <CircularProgress />
+      </div>
+    )
+  }
+  if (!segment) {
+    return (
+      <div>
+        <div className={classes.verticalSpace} />
+        <div className={classes.header}>
+          <p>{getString('welcome_title')}</p>
         </div>
-      )
-    }
-    if (!segment) {
-      return (
-        <div>
-          <div className={classes.verticalSpace}/>
-          <div className={classes.header}>{getString('welcome_title')}</div>
-          <div className={classes.subheader}>{getString('welcome_subtitle')}</div>
-          <div className={classes.subheader}>{getString('welcome_subtitle_2')}</div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_subtitle')}</p>
         </div>
-
-      )
-    }
-    return <SegmentForm
-        segment={segment}
-        onChanged={onSegmentChanged}
-        onValidationFailed={onValidationFailed}
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hints')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hint_1')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hint_2')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hint_3')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hint_4')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_hint_5')}</p>
+        </div>
+        <div className={classes.subheader}>
+          <p>{getString('welcome_to_howto')}</p>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <SegmentForm
+      segment={segment}
+      onChanged={onSegmentChanged}
+      onValidationFailed={onValidationFailed}
     />
+  )
 }
 
 RightPanel.propTypes = {
-    isLoading: PropTypes.bool.isRequired,
-    selectedSegment: PropTypes.object,
-    onSegmentChanged: PropTypes.func.isRequired,
-    setAlertDisplayed: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  selectedSegment: PropTypes.object,
+  onSegmentChanged: PropTypes.func.isRequired,
+  setAlertDisplayed: PropTypes.func.isRequired,
 }
 
 export default RightPanel
--- src/strings.js
+++ src/strings.js
@@ -1,51 +1,62 @@
-
 const strings = {
-    // Start page
-    welcome_title: () => 'Willkommen bei ParkplatzTransform',
-    welcome_subtitle: () => 'Wähle einen vorhandenen Abschnitt oder erstelle einen Neuen',
-    welcome_subtitle_2: () => 'Zoome in die Karte um die Bearbeitungswerkzeuge zu sehen',
+  // Start page
+  welcome_title: () => 'Willkommen bei ParkplatzTransform',
+  welcome_subtitle: () => 'Dies ist ein Tool zum Erfassen von Parkplätzen.',
+  welcome_hints: () => 'So funktioniert das Eintragen: ',
+  welcome_hint_1: () =>
+    'Lege einen Account an, indem du dich mit deiner E-MailAdresse einloggst – ein Passwort ist nicht nötig.',
+  welcome_hint_2: () =>
+    'Um Zählergebnisse einzutragen, wähle zunächst einenvorhandenen Abschnitt aus oder erstelle einen neuen.',
+  welcome_hint_3: () =>
+    'Zoome dafür in die Karte. Rechts oben siehst du Icons für dieBearbeitungswerkzeuge, die du zum Abschnitt einzeichnenbenötigst.',
+  welcome_hint_4: () =>
+    'Um nun in dem ausgewählten Abschnitt die gezähltenParkplätze zu erfassen, charakterisiere Unterabschnitte und definiere Parkplatztypen. Um den Aufwand gering zu halten, kannst du ganze Unterabschnitte kopieren, falls ein Parkplatzmuster mehr als einmal auftaucht.',
+  welcome_hint_5: () =>
+    'Es findet eine automatische Sicherung der eingetragenenDaten statt. Speichern und aktives Ausloggen ist nichtnotwendig.',
+  welcome_to_howto: () =>
+    'Hier geht es zu einer detaillierten Nutzer*innenanleitung',
 
-    // Alert success messages
-    segment_create_success: () => 'Abschnitt erfolgreich erstellt.',
-    segment_update_success: () => `Abschnitt erfolgreich gespeichert.`,
-    segment_delete_success: (num) => {
-        if (num === 1) {
-            return 'Abschnitt gelöscht.'
-        }
-        return `${num} Abschnitte gelöscht.`
-    },
-    segment_loaded_success: () => 'Abschnitte erfolgreich geladen.',
+  // Alert success messages
+  segment_create_success: () => 'Abschnitt erfolgreich erstellt.',
+  segment_update_success: () => `Abschnitt erfolgreich gespeichert.`,
+  segment_delete_success: (num) => {
+    if (num === 1) {
+      return 'Abschnitt gelöscht.'
+    }
+    return `${num} Abschnitte gelöscht.`
+  },
+  segment_loaded_success: () => 'Abschnitte erfolgreich geladen.',
 
-    segment_delete_confirm: () => 'Segment löschen?',
+  segment_delete_confirm: () => 'Segment löschen?',
 
-    // Alert failure messages
-    segment_create_failure: () => 'Abschnitt konnte nicht erstellt werden.',
-    segment_update_failure: () => 'Abschnitt konnte nicht gespeichert werden.',
-    subsegment_invalid: () => 'Mindest ein Unterabschnitt ist fehlerhaft.',
-    segment_delete_failure: (num) => {
-        if (num === 1) {
-            return "Abschnitt konnte nicht gelöscht werden."
-        }
-        return `${num} Abschnitte konnte nicht gelöscht werden.`
-    },
-    segment_loaded_failure: () => 'Beim Laden der Abschnitte trat ein Fehler auf.',
+  // Alert failure messages
+  segment_create_failure: () => 'Abschnitt konnte nicht erstellt werden.',
+  segment_update_failure: () => 'Abschnitt konnte nicht gespeichert werden.',
+  subsegment_invalid: () => 'Mindest ein Unterabschnitt ist fehlerhaft.',
+  segment_delete_failure: (num) => {
+    if (num === 1) {
+      return 'Abschnitt konnte nicht gelöscht werden.'
+    }
+    return `${num} Abschnitte konnte nicht gelöscht werden.`
+  },
+  segment_loaded_failure: () =>
+    'Beim Laden der Abschnitte trat ein Fehler auf.',
 
-    // Download & Save buttons
-    download_geo_json: () => 'Daten herunterladen',
-    download_visible_segments: () => 'GeoJSON - sichtbaren Bereich',
-    download_all_segments: () => 'GeoJSON - alle geladenen Bereiche',
-    save: () => 'Speichern',
+  // Download & Save buttons
+  download_geo_json: () => 'Daten herunterladen',
+  download_visible_segments: () => 'GeoJSON - sichtbaren Bereich',
+  download_all_segments: () => 'GeoJSON - alle geladenen Bereiche',
+  save: () => 'Speichern',
 
-    // Form validation helper text
-    helper_text_length: () => 'Länge oder Stellplatzzahl muss gesetzt werden.',
+  // Form validation helper text
+  helper_text_length: () => 'Länge oder Stellplatzzahl muss gesetzt werden.',
 }
 
-
 export default function getString(key, variable) {
-    try {
-        return strings[key](variable)
-    } catch {
-        console.error("missing translation key for:", key)
-        return key
-    }
+  try {
+    return strings[key](variable)
+  } catch {
+    console.error('missing translation key for:', key)
+    return key
+  }
 }
