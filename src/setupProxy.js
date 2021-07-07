8cd3884d396e90710356d0fa81472da1988a0df5
Theo <4714866+theolampert@users.noreply.github.com>
2021-07-07 20:55:13 +0200

    Update proxy endpoints

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['^/segments/*', '^/users/*'],
+    ['^/query-segments/', '^/segments/*', '^/users/*'],
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL || 'https://api.xtransform.org',
       changeOrigin: true,
