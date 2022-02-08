2ef31464bbdd04b9dc8e28a1e8cea083b462735a
Theo Lampert <mail@theo.sh>
2022-02-08 09:32:51 +0100

    Add cluster path to local proxy, add geolocate control

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['^/query-segments/', '^/segments/*', '^/users/*'],
+    ['^/query-segments/', '^/segments/*', '^/users/*', '/clusters/*'],
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL || 'https://api.xtransform.org',
       changeOrigin: true,
